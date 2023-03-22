d1abd28a1b84f99c238728ed27000844d381e5c5
naachopedraza1 <nachi_choco@live.com.ar>
2023-03-22 02:18:13 -0300

    view guide v4

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 
-import { App } from './App'
+import { Provider } from 'react-redux/es/exports';
+
 import { AppTheme } from './theme/AppTheme';
+import { App } from './App'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  /*   <React.StrictMode> */
-  <BrowserRouter>
-    <AppTheme>
-      <App />
-    </AppTheme>
-  </BrowserRouter>
-  /* </React.StrictMode>, */
+  <React.StrictMode>
+    <BrowserRouter>
+      <AppTheme>
+        <App />
+      </AppTheme>
+    </BrowserRouter>
+  </React.StrictMode>
 )
